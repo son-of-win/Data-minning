ba1cc0d3cd6d7b54c79db8a8008bc222ad61ddd2
son-of-win <60661269+son-of-win@users.noreply.github.com>
2020-08-06 15:31:23 +0700

    Update run_crawler.py

--- run_crawler.py
+++ run_crawler.py
@@ -3,3 +3,5 @@ from scrapy.cmdline import execute
 if __name__ == '__main__':
     execute('scrapy crawl phimmoi'.split())  # crawl file phimmoi.py
     #execute('scrapy crawl dantri'.split()) # crawl file dantri.py
+    #execute('scrapy crawl amazone'.split()) # crawl file amazone.py
+    
